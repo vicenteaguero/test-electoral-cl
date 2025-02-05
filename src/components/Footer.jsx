ff9e421db761a83cdc26722edc2bc5aa184e74f3
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 09:00:20 -0300

    feat(website): :sparkles: Added Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.jsx
+
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Test Electoral Chile</p>
+    </footer>
+  );
+};
+
+export default Footer;
