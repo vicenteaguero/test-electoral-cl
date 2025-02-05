ecddc45d084115d7b9f84957871651c9b206390c
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 09:01:11 -0300

    feat(website): :sparkles: Added placeholder for about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// src/pages/About.jsx
+
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h2>Acerca de</h2>
+      <p>Información sobre el propósito del sitio.</p>
+    </div>
+  );
+};
+
+export default About;
