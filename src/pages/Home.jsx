6416a8054b532ea12500bbaf7744aaf0f51f690e
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 09:01:27 -0300

    feat(website): :sparkles: Added placeholder for home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Bienvenido a Test Electoral Chile</h2>
+      <p>Conoce a tus candidatos y elige informado.</p>
+    </div>
+  );
+};
+
+export default Home;
